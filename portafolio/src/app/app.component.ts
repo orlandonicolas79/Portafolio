52957c9f2baa60346579d25e1c2d838cf52382ff
Nico <orlandonicolas79@gmail.com>
2022-07-10 23:24:51 -0300

    Implementación del Framework "Angular"

---
+++ portafolio/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'portafolio';
+  descripcion = 'Ésta es una prueba del portafolio. Un momento de dolor vale toda una vida de gloria.';
+}
